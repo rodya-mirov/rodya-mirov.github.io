80f58bbceb4d786bb7ccb69f9c7282bdaed1214e
Richard Rast <Richard.Rast@trinetx.com>
2016-09-28 08:12:32 -0400

    Set up .gitattributes and updated some tenses (was, etc.). Disabled CV tab (but the code still exists)

--- content/tab_content.js
+++ content/tab_content.js
@@ -1,1 +1,1 @@
-document.write("<script src=\"content/home.js\"></script>");document.write("<script src=\"content/games.js\"></script>");document.write("<script src=\"content/research.js\"></script>");document.write("<script src=\"content/talks.js\"></script>");document.write("<script src=\"content/cv.js\"></script>");
+document.write("<script src=\"content/home.js\"></script>");document.write("<script src=\"content/games.js\"></script>");document.write("<script src=\"content/research.js\"></script>");document.write("<script src=\"content/talks.js\"></script>");
